55cfeaccdf8d9b7be189a829c9144a7d60f645d9
AliBYKER <buyukerali@gmail.com>
2022-10-11 12:55:43 +0300

    ı finished my second task.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,6 +2,16 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            Merhaba();
+            System.out.print(i);
+            System.out.println();
 
+        }
+
+    }
+
+    public static void Merhaba(){
+        System.out.print("Merhaba ");
     }
 }
